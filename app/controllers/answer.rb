93f4b811cb416f18ecebc62f4916a344378bdc6d
Daniel Yoon & Melissa Rudofsky <pair+dyoon3102.mrudof@devbootcamp.com>
2016-08-31 15:24:35 -0400

    added post method for creating new answer

--- app/controllers/answer.rb
+++ app/controllers/answer.rb
@@ -1,10 +1,21 @@
-get '/answers' do
-end
-
-get '/answer/new' do
+get 'question/:id' do
+  if login?
+    erb :'answers/new'
+  end
 end
 
 post '/answers' do
+  @answer = Answer.new(text: params[:text], user_id: sessions[:user_id], question_id: params[:id])
+  if @answer.save
+    if response.xhr?
+      JSON.generate(@answer)
+    end
+    redirect "/questions/#{params[:id]}"
+  else
+    @errors = @answer.errors.full_messages
+    erb :'answers/new'
+  end
+
 end
 
 
--- public/js/application.js
+++ public/js/application.js
@@ -1,9 +1,11 @@
 $(document).ready(function () {
 
-  // PSEUDO-CODE:
-  //   1- intercept the form submission event using jQuery
-  //   2- prevent the default action for that event from happening
-  //   3- use jQuery to submit an AJAX post to the form's action
-  //   4- when the AJAX post is done, display the new die roll using jQuery
+  $('#new-answer-submit').submit(function(event){
+    $event.preventDefault;
+    $.ajax({
+      method: 'post',
+      url: '/answers'
+    })
+  });
 
 });
