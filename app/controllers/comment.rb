046b6d21f23bdce62953099c3f15e09b5b21c83f
Margaret Kamimoto & Kiren Srinivasan <pair+mkamimoto.tuneguru@devbootcamp.com>
2016-09-01 11:51:52 -0400

    Almost finished with comments and ajaxified comments; need to debug errors not showing when comment doesn't have at least 10 characters

--- app/controllers/comment.rb
+++ app/controllers/comment.rb
@@ -1,14 +1,49 @@
 get '/questions/:question_id/comments/new' do
   @question = Question.find(params[:question_id])
-  erb :'comments/question/new'
+  if request.xhr?
+    erb :'comments/question/new', layout: false
+  else
+    erb :'comments/question/new'
+  end
 end
 
-post '/comments' do
+get '/questions/:question_id/answers/:answer_id/comments/new' do
+  @question = Question.find(params[:question_id])
+  @answer = @question.answers.find(params[:answer_id])
+  if request.xhr?
+    erb :'comments/answer/new', layout: false
+  else
+    erb :'comments/answer/new'
+  end
+end
+
+# POST request for comments on questions
+post '/question/comments' do
   @comment = Comment.new(params[:comment])
   if @comment.save
-    redirect "/questions/#{@comment.commentable_id}"
+    if request.xhr?
+      erb :'comments/_newly_added_comment', layout: false
+    else
+      redirect "/questions/#{@comment.commentable_id}"
+    end
   else
     @errors = @comment.errors.full_messages
     erb :'comments/question/new'
   end
 end
+
+# POST request for comments on answers
+post '/answer/comments' do
+  @comment = Comment.new(params[:comment])
+  if @comment.save
+    if request.xhr?
+      erb :'comments/_newly_added_comment', layout: false
+    else
+      redirect "/questions/#{@comment.commentable_id}"
+    end
+  else
+    @errors = @comment.errors.full_messages
+    erb :'comments/answer/new'
+  end
+end
+
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,6 @@
+// Answer-based AJAX
 $(document).ready(function () {
-
   $('#answer-form').submit(function(event){
-
     event.preventDefault();
     var input = $(this).serialize();
     $.ajax({
@@ -16,5 +15,78 @@ $(document).ready(function () {
     $('#answer-errors').remove();
     $('#answer-text').val("");
   });
+});
 
+// Question-based Response-based AJAX
+$(document).ready(function () {
+  $('#new-question-comment-link').click(function (event) {
+    event.preventDefault();
+    var request = $.ajax({
+      url: $(event.target).attr("href"),
+      method: "GET"
+    });
+
+    request.done(function (response) {
+      // debugger;
+      $(event.target).hide();
+      $(event.target).after(response);
+    });
+  });
+
+  $('#new-question-comment-container').on("submit", "#new-question-comment-form", function(event) {
+    event.preventDefault();
+    var input_data = $(this).serialize();
+
+    var request = $.ajax({
+      url: $(event.target).attr("action"),
+      method: $(event.target).attr("method"),
+      data: input_data
+    });
+
+    request.done(function (response) {
+      $(event.target).hide();
+      $("#question-comments").append(response);
+      var addCommentLink = $(event.target).closest("#question-comments").find("#new-question-comment-link");
+      $(event.target).closest("#question-comments").find("#new-question-comment-link").remove();
+      $("#question-comments").append(addCommentLink);
+      $(addCommentLink).show();
+    });
+  });
+});
+
+// Answer-based Response-based AJAX
+$(document).ready(function () {
+  $('#new-answer-comment-link').click(function (event) {
+    event.preventDefault();
+    var request = $.ajax({
+      url: $(event.target).attr("href"),
+      method: "GET"
+    });
+
+    request.done(function (response) {
+      debugger;
+      $(event.target).hide();
+      $(event.target).after(response);
+    });
+  });
+
+  $('#new-answer-comment-container').on("submit", "#new-answer-comment-form", function(event) {
+    event.preventDefault();
+    var input_data = $(this).serialize();
+
+    var request = $.ajax({
+      url: $(event.target).attr("action"),
+      method: $(event.target).attr("method"),
+      data: input_data
+    });
+
+    request.done(function (response) {
+      $(event.target).hide();
+      $("#answer-comments").append(response);
+      var addCommentLink = $(event.target).closest("#answer-comments").find("#new-answer-comment-link");
+      $(event.target).closest("#answer-comments").find("#new-answer-comment-link").remove();
+      $("#answer-comments").append(addCommentLink);
+      $(addCommentLink).show();
+    });
+  });
 });
